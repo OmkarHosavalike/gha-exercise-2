63aa4750659cd4859d8b6e798f8514ac5140d944
OmkarHosavalike <homkar8@gmail.com>
2025-10-28 15:11:17 +0530

    add lorem in dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <p>Lorem, ipsum dolor.</p>
       </header>
       <MainContent />
     </>
